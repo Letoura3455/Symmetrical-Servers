0246367997538323a979f6b07dac161577562e5b
Letoura3455 <105031389+Letoura3455@users.noreply.github.com>
2022-05-13 14:44:05 -0500

    Update library.lua

--- main/library.lua
+++ main/library.lua
@@ -1,5 +1,5 @@
 pcall(function()
-    game:GetService('CoreGui')['bluhub']:Destroy()
+    game:GetService('CoreGui')['Symmetrical Servers']:Destroy()
 end)
 
 if not getgenv().theme then getgenv().theme = Color3.fromRGB(255,65,65) end
